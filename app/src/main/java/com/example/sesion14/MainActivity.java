2e584ab0b0fb78becdd96a8cd577b263bcde8f2e
NerioCH <huarcayanerio@gmail.com>
2022-12-21 18:40:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sesion14/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.sesion14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtUsu, txtPass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtUsu = findViewById(R.id.txtUsuario);
+        txtPass = findViewById(R.id.txtPassword);
+    }
+
+    public void Consulta(View view){
+        try {
+            Statement st=conexionBD().createStatement();
+            ResultSet rs=st.executeQuery("SELECT * FROM usuarios where codigo='"+txtUsu.getText().toString()+"'");
+            if(rs.next()){
+
+                Toast.makeText(getApplicationContext(),"Conexion establecida",Toast.LENGTH_SHORT).show();
+            }
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public Connection conexionBD(){
+        Connection cnn = null;
+        try {
+            StrictMode.ThreadPolicy pol=new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(pol);
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            cnn = DriverManager.getConnection("jdbc:jtds:sqlserver://169.254.57.117:1433/DBProyecto;"+
+                    "instance=MSSQLSERVER;user=sa;password=nerio123");
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+        return cnn;
+    }
+}
